e13953a846f91927672cfe04a4d14be829fd755b
RaulToledoProcopio <rtolpro@gmail.com>
2025-02-20 20:16:23 +0100

    Prueba Login

--- src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
+++ src/main/kotlin/com/es/aplicacion/controller/UsuarioController.kt
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -30,8 +29,6 @@ class UsuarioController {
     private lateinit var tokenService: TokenService
     @Autowired
     private lateinit var usuarioService: UsuarioService
-
-    @GetMapping("/")
 
     @PostMapping("/register")
     fun insert(
